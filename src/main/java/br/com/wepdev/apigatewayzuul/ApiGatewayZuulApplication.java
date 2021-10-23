6fbaea0f5d2002139f9b56b509035f52cc4c9e3b
wep1980 <wepbike@gmail.com>
2021-10-23 07:50:28 -0300

    Pirmeiro commit, projeto criado

---
+++ src/main/java/br/com/wepdev/apigatewayzuul/ApiGatewayZuulApplication.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.apigatewayzuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayZuulApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/wepdev/apigatewayzuul/ApiGatewayZuulApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.wepdev.apigatewayzuul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayZuulApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
