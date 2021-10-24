03059d4c2b41788eb7c286c028773dd7d163df43
wep1980 <wepbike@gmail.com>
2021-10-24 06:24:48 -0300

    Configurando Zuul api gateway e timeout

--- src/main/java/br/com/wepdev/apigatewayzuul/ApiGatewayZuulApplication.java
+++ src/main/java/br/com/wepdev/apigatewayzuul/ApiGatewayZuulApplication.java
@@ -2,7 +2,11 @@ package br.com.wepdev.apigatewayzuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableEurekaClient // Configurando para ser um cliente eureka
+@EnableZuulProxy // Configurando o proxy do Zuul
 @SpringBootApplication
 public class ApiGatewayZuulApplication {
 
